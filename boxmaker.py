ffc331b65b8d4d17d2af051cf3db9837f2a84887
Ryan Jarvis <ryan@shopboxretail.com>
2019-02-18 15:25:52 -0800

    Misc PEP8

--- boxmaker.py
+++ boxmaker.py
@@ -36,7 +36,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-__version__ = "0.94"  ### please report bugs, suggestions etc at https://github.com/paulh-rnd/TabbedBoxMaker ###
+__version__ = "0.94"  # please report bugs, suggestions etc at https://github.com/paulh-rnd/TabbedBoxMaker ###
 
 import gettext
 import math
